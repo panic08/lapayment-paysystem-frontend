74a573ad1731ea2bc4773876a3b72c4859b0747e
panic08 <andrey1010102008@gmail.com>
2023-05-05 22:17:18 +0300

    v1.0

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import indexStyle from "styles/index.module.scss";
+
 export default function Home() {
-  return (
-      <>
-      <h1>Lapayment!</h1>
-      </>
-  )
+    return (
+        <div>
+            <header className={indexStyle.header}>
+                <nav className={indexStyle.nav}>
+                    <a href="/" className={indexStyle.logo}>
+                        <img src="/images/img.png" alt="Your custom logo" />
+                    </a>
+                    <ul className={indexStyle.menu}>
+                        <li><a href="/fsdfsd">Главная</a></li>
+                        <li><a href="#">Документация</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">GitHub</a></li>
+                    </ul>
+                    <div className={indexStyle.buttons}>
+                        <button className={indexStyle.signIn}>Авторизация</button>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    );
 }
